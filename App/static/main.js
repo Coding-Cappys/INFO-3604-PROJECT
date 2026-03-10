0cc2ea52c089c86547736c2c834534263b2a674a
Reon-new <retonz.23@gmail.com>
2026-03-10 01:43:14 -0400

    added reviewer designs, updated main layout template and one reviewer template, organized file navigation for easier accessing

--- App/static/main.js
+++ App/static/main.js
@@ -1,12 +1,12 @@
 
-async function getUserData(){
+async function getUserData() {
     const response = await fetch('/api/users');
     return response.json();
 }
 
-function loadTable(users){
+function loadTable(users) {
     const table = document.querySelector('#result');
-    for(let user of users){
+    for (let user of users) {
         table.innerHTML += `<tr>
             <td>${user.id}</td>
             <td>${user.username}</td>
@@ -14,7 +14,7 @@ function loadTable(users){
     }
 }
 
-async function main(){
+async function main() {
     const users = await getUserData();
     loadTable(users);
 }
